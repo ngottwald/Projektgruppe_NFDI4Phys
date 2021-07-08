16a3717cd030affd6e7edfad8d18e6340f2d0384
ngottwald <nicolas.gottwald@student.uni-siegen.de>
2021-07-08 17:48:38 +0200

    added jena libraries

--- RDFWorker/src/adapter/Adapter.java
+++ RDFWorker/src/adapter/Adapter.java
@@ -1,5 +1,25 @@
 package adapter;
 
 public class Adapter {
+	
+	public Adapter() {
+		super();
+	}
+	
+	/**
+	 * Gets an input file and generates a rdf file.
+	 * @param file the input filepath.
+	 */
+	public void ParseInput(String file) {
+		
+	}
+	
+	/**
+	 * Gets a file destination and produces an output.
+	 * @param file the output filepath.
+	 */
+	public void ExportAs(String file) {
+		
+	}
 
 }
---
+++ RDFWorker/src/adapter/ParseRdf.java
@@ -0,0 +1,21 @@
+package adapter;
+
+public class ParseRdf {
+	
+	/**
+	 * Parses a rdf file to java classes.
+	 * @param file the rdf filepath.
+	 */
+	public ParseRdf(String file) {
+		parse(file);
+	}
+	
+	/**
+	 * Parses the rdf file.
+	 * @param file the rdf filepath.
+	 */
+	public void parse(String file) {
+		
+	}
+
+}
