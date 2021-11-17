d08178cd9b72b871ea47433c8dc73d143e0c8617
Josef Matwich <josef95@outlook.de>
2021-11-17 11:42:29 +0100

    Bring some structure and add first test.

--- pyCaosDB/main.py
+++ pyCaosDB/main.py
@@ -9,10 +9,25 @@ def main():
     conf = dict(db.configuration.get_config().items("Connection"))
     print("##### Config:\n{}\n".format(conf))
 
-
+    # Query the server, the result is a Container
+    # result = db.Query("FIND Record").execute()
+    # print("##### First query result:\n{}\n".format(result[0]))
 
 
 if __name__ == '__main__':
     main()
+    # db.Info()
+    # a = db.Property(name="a", datatype=db.DOUBLE)
+    # container = db.Container()
+    # container.extend([a])
+    # container.insert()
+    # result = db.Query("FIND Record").execute()
+    # print("##### First query result:\n{}\n".format(result[0]))
+    # guitar_record = find_guitar()
+    # print(guitar_record)
+
+    # analyse()
 
     rdf_worker1 = rdf_worker.RDFWorker("RDFFiles/NFDI4Phys.owl")
+    #rdf_worker1.show_rdf_graph()
+    rdf_worker1.export_caosdb_data_model(db)
--- pyCaosDB/rdf_worker.py
+++ pyCaosDB/rdf_worker.py
@@ -1,14 +1,21 @@
 import rdflib
+from rdflib import URIRef, Literal, BNode
 
 
 class RDFWorker:
+    __doc__ = """
+    """
 
     def __init__(self, rdf_file_path):
         self.g = rdflib.Graph()
+        self.literals = []
 
         if rdf_file_path:
             result = self.g.parse(rdf_file_path)
 
+    """
+
+    """
     def show_rdf_graph(self):
         # loop through each triple in the graph (subj, pred, obj)
         for subj, pred, obj in self.g:
@@ -22,10 +29,48 @@ class RDFWorker:
 
         # print out the entire Graph in the RDF Turtle format
         # print(self.g.serialize(format="turtle").decode("utf-8"))
+    
+    def handle_Literal(self, literal):
+        datatype = self.get_datatype_of_Literal(literal)
+        value = literal.value
+        
+        if not (value, datatype) in self.literals:
+            self.literals.append((value, datatype))
+
+    def get_value_of_Literal(self, literal):
+        return literal.value
+    
+    def get_datatype_of_Literal(self, literal):
+        if type(literal.datatype) == None:
+            if literal.datatype.find('integer') > -1:
+                return 'INT'
+            elif literal.datatype.find('nonNegativeInteger') > -1:
+                return 'UNSIGNED INT'
+            else:
+                return type(literal.value)   
+        else:
+            return type(literal.value)     
 
     def export_caosdb_data_model(self, db):
-        for subj, pred, obj in self.g:
-            print("Subject: " + subj);
-            print("Pred: " + pred);
-            print("Obj: " + obj);
-        # TODO: Modify the db using the subj, pred, obj
+        Camera = URIRef("http://www.semanticweb.org/tobiasvente/ontologies/2020/11/NFDI4Phys#Camera")
+        subPropOf = URIRef("http://www.w3.org/2000/01/rdf-schema#subPropertyOf")
+        isType = URIRef("http://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+
+        count_test_sub = 0
+
+        subject_list = []
+        pred_list = []
+        for idx, (subj, pred, obj) in enumerate(self.g):
+
+            if type(obj) == Literal:
+                self.handle_Literal(obj)
+            else:
+                print(f'------> {obj} --- OF TYPE {type(obj)} --')
+        
+        print('All literals')
+        for value, datatype in self.literals:
+            print(f'value: {value}   datatype: {datatype}')
+        # container = db.Container()
+
+
+
